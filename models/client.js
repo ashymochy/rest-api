09688072cc0282289ad928618ebfcab5f8e04b21
ashymochy <101075664+ashymochy@users.noreply.github.com>
2023-09-02 21:25:12 +0800

    Update client.js

--- models/client.js
+++ models/client.js
@@ -4,8 +4,8 @@ const clientSchema = new mongoose.Schema({
   firstName: String,
   lastName: String,
   age: Number,
-  birthday: Date, // Use Date data type for birthday
-  phoneNumber: String, // Use String for phone number
+  birthday: Date,
+  phoneNumber: String,
 });
 
-module.exports = mongoose.model('Client', clientSchema); // Use 'Client' as the model name
+module.exports = mongoose.model('Client', clientSchema); 
